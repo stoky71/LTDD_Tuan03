6c6cdf8d49b3a5ce4e5f5201182142075d443ef1
stoky71 <10lop96phungbuitronghieu@gmail.com>
2022-03-04 10:50:15 +0700

    update

---
+++ app/src/main/java/com/example/lap03/src01.java
@@ -0,0 +1,14 @@
+package com.example.lap03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class src01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_src01);
+    }
+}
